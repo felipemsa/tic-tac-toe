06d412c0ea7321735048001f8708dd3a27e0f83c
felipemsa <felipe.fulgur@gmail.com>
2017-07-10 00:52:26 -0300

    inicio da AI

---
+++ app/src/main/java/com/felipemsa/tictactoe/model/AIMove.java
@@ -0,0 +1,8 @@
+package com.felipemsa.tictactoe.model;
+
+/**
+ * Created by Felipe Almeida on 07/10/2017.
+ */
+
+public class AIMove {
+}
---
+++ app/src/main/java/com/felipemsa/tictactoe/util/MoveChooser.java
@@ -0,0 +1,19 @@
+package com.felipemsa.tictactoe.util;
+
+import com.felipemsa.tictactoe.model.AIMove;
+import com.felipemsa.tictactoe.model.Choice;
+
+/**
+ * Created by Felipe Almeida on 07/10/2017.
+ */
+
+public class MoveChooser {
+
+	public static AIMove choose(Choice choice, Choice hash[][]) {
+
+
+
+		return null;
+	}
+
+}
